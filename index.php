6303398695dbdff6a079a726435667685e751cdb
mozgbrasil <mozgbrasil@gmail.com>
2017-04-27 11:46:23 -0300

    commit - 27/04/2017 11:45:39

--- index.php
+++ index.php
@@ -8,4 +8,25 @@ require 'vendor/autoload.php';
 
 echo '<pre>';
 
-print_r(spl_autoload_functions());exit;
+//print_r(spl_autoload_functions());
+
+//
+
+
+/*db: heroku_5714b88b9ab57b4
+Username:	b808728448ca7e
+Password:	2c744f8b
+*/
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+print_r($url);
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+print_r($conn);
